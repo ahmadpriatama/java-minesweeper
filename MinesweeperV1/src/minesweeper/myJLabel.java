0f4dc9ccf7b49a3b6fe8fe595b528cc27f3bdd1d
ahmadpriatama <ahmadpriatama@d3047ad8-6cf2-41f6-a9f6-39387f14438d>
2010-12-25 18:06:49 +0000

    buat array "mine"nya
    git-svn-id: http://svn.code.sf.net/p/java-minesweepe/code/trunk@3 d3047ad8-6cf2-41f6-a9f6-39387f14438d

---
+++ MinesweeperV1/src/minesweeper/myJLabel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package minesweeper;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Ahmad
+ */
+public class myJLabel extends JLabel{
+
+    public myJLabel() {
+        super();
+
+    }
+}
